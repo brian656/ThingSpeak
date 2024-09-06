fa260ba382afc7688a03f27e6bf7ecb110008eea
brian656 <62913228+brian656@users.noreply.github.com>
2024-09-06 18:49:00 -0500

    Update config.h

--- config.h
+++ config.h
@@ -1,2 +1,3 @@
+//pongan datos de su red, la red que se usa aca es de un celular asdemas ya se cambio la contraseña ;)
 const char* ssid     = "Redmi Note 12 Pro+ 5G";
 const char* password = "Hola1111";
